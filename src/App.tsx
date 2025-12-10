9aa3e9eeac1a82dcbaf695d721d4bd5403aec1c2
mizgithub <mizanu143@gmail.com>
2025-12-10 11:14:22 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+      <div className='w-full bg-zinc-900 p-0 m-0'>
+          something goes here
+       </div>
+  )
+}
+
+export default App
